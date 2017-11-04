c06e61167ffe0f5b733b62aec2c23543d02a0115
Pedro Henrique Pacheco <pedropacheco92@outlook.com>
2017-11-04 11:29:18 -0200

    wip

--- src/h1Pacman/PacMan.java
+++ src/h1Pacman/PacMan.java
@@ -2,22 +2,19 @@ package h1Pacman;
 
 import javax.swing.JFrame;
 
-
 @SuppressWarnings("serial")
-public class PacMan extends JFrame
-{
+public class PacMan extends JFrame {
 
-  public PacMan()
-  {
-    add(new Board());
-    setTitle("Pacman");
-    setDefaultCloseOperation(EXIT_ON_CLOSE);
-    setSize(380, 420);
-    setLocationRelativeTo(null);
-    setVisible(true);
-  }
+	public PacMan()	{
+		add(new Board());
+		setTitle("Pacman");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(380, 420);
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
 
-  public static void main(String[] args) {
-      new PacMan();
-  }
+	public static void main(String[] args) {
+		new PacMan();
+	}
 }
